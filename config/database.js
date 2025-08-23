eecfad87c158d1d8fc7999d8725d6f5741bf715d
Richard Baine <efdeliveryservice@gmail.com>
2025-08-23 19:47:17 -0400

    update

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT,
-  storage: process.env.DB_STORAGE, // For SQLite
-});
-
-module.exports = sequelize;
