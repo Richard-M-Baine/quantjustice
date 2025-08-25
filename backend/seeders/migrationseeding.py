a12a8536baaa2fb538c647c8b4eba6aa9e3cc43f
Richard Baine <efdeliveryservice@gmail.com>
2025-08-25 12:12:30 -0400

    plugging along

--- backend/seeders/migrationseeding.py
+++ backend/seeders/migrationseeding.py
@@ -2,10 +2,10 @@ import pandas as pd
 import sqlite3
 
 # Read CSV
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('newOne.csv')
 
 # Connect to database
-conn = sqlite3.connect('your_database.db')
+conn = sqlite3.connect('myapp.db')
 
 # Import to SQLite (if_exists='replace' will overwrite existing data)
 df.to_sql('Things', conn, if_exists='append', index=False)
